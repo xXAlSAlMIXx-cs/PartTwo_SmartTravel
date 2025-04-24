156da687c5cb85887690c36c6e68449a63507d1e
tariq <tariqs.om79@gmail.com>
2025-04-24 16:15:12 +0400

    import new package

--- part1_project/lib/loginPage.dart
+++ part1_project/lib/loginPage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -48,42 +49,94 @@ class LoginPage extends StatelessWidget {
                         color: Colors.white,
                         borderRadius: BorderRadius.only(
                             topLeft: Radius.circular(60),
-                            topRight: Radius.circular(60))),
+                            topRight: Radius.circular(60)
+
+                        ),
+                              ),
                     child: Padding(
                       padding: const EdgeInsets.fromLTRB(40, 90, 40, 200),
                       child: Expanded(
                           child: Container(
-                        decoration: BoxDecoration(
-                            color: Colors.red,
-                            borderRadius: BorderRadius.circular(10)),
                         child: Form(
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.start,
                             children: [
                               Padding(
                                 padding: const EdgeInsets.all(14),
-                                child: TextFormField(
-                                  decoration: InputDecoration(
-                                      labelText: 'Username',
-                                      border: OutlineInputBorder()),
-                                  validator: (value) => value!.isEmpty
-                                      ? 'Please enter your username'
-                                      : null,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: Colors.white70,
+                                      borderRadius: BorderRadius.circular(10),
+                                      boxShadow: [
+                                        BoxShadow(
+                                            color: Colors.grey.shade400,
+                                            spreadRadius: 8,
+                                            blurRadius: 10,
+                                            offset: Offset(4, 4))
+                                      ]),
+                                  child: TextFormField(
+                                      decoration: InputDecoration(
+                                    labelText: 'UserName',
+                                          filled: true,
+                                          fillColor: Colors.grey[200], // background color like in the image
+                                          enabledBorder: OutlineInputBorder(
+                                            borderRadius: BorderRadius.circular(10), // rounded border
+                                            borderSide: BorderSide(color: Colors.deepPurple),), // border color
+                                    focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(30),
+                                      borderSide: BorderSide(
+                                          color: Colors.deepPurple, width: 2),
+                                    ),
+                                  )),
                                 ),
                               ),
-                              SizedBox(height: 20),
-                              TextFormField(
-                                decoration:
-                                    InputDecoration(labelText: 'Password'),
-                                obscureText: true,
-                                validator: (value) => value!.isEmpty
-                                    ? 'Please enter your password'
-                                    : null,
+                              Padding(
+                                padding: const EdgeInsets.all(14),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: Colors.white70,
+                                      borderRadius: BorderRadius.circular(10),
+                                      boxShadow: [
+                                        BoxShadow(
+                                            color: Colors.grey.shade400,
+                                            spreadRadius: 8,
+                                            blurRadius: 10,
+                                            offset: Offset(4, 4))
+                                      ]),
+                                  child: TextFormField(
+                                      decoration: InputDecoration(
+                                    labelText: 'Password',
+                                          filled: true,
+                                          fillColor: Colors.grey[200], // background color like in the image
+                                          enabledBorder: OutlineInputBorder(
+                                            borderRadius: BorderRadius.circular(10), // rounded border
+                                            borderSide: BorderSide(color: Colors.deepPurple),), // border color)
+                                    focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(30),
+                                      borderSide: BorderSide(
+                                          color: Colors.deepPurple, width: 2),
+                                    ),
+                                  )),
+                                ),
                               ),
+
+
                               SizedBox(height: 30),
-                              ElevatedButton(
-                                onPressed: () {},
-                                child: Text('Login'),
+                              SizedBox(
+
+                                width: double.infinity,
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.orange, // Button background color
+                                    foregroundColor: Colors.white,  // Text (label) color
+                                    padding: EdgeInsets.symmetric(vertical: 16), // Optional: makes it taller
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30), // Optional: rounded corners
+                                    ),
+                                  ),
+                                  onPressed: () {},
+                                  child: Text('Login',style:TextStyle(fontWeight: FontWeight.bold),),
+                                ),
                               ),
                             ],
                           ),
